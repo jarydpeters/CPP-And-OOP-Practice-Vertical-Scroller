2945cab412afe8c73b1ba3a5309e4a3a6527a0b7
KATESPC\artif <jaryd.peters@ndsu.edu>
2024-12-21 20:39:59 -0600

    continues pushing for OpenGL rendering

--- include/headers/utilities/openGLUtility.h
+++ include/headers/utilities/openGLUtility.h
@@ -8,6 +8,7 @@
 
 #include "assetFilePaths.h"
 #include "shaderUtility.h"
+#include "stb_image.h"
 
 void initOpenGL(SDL_Window* window);
 void cleanupOpenGL();
@@ -15,5 +16,7 @@ void renderQuadWithTexture(GLuint textureID, const SDL_Rect& rect);
 
 extern SDL_GLContext glContext;
 extern GLuint shaderProgram;
+extern GLuint VAO;
+extern GLuint VBO;
 
 #endif //OPENGLUTILITY_H
--- src/utilities/openGLUtility.cpp
+++ src/utilities/openGLUtility.cpp
@@ -2,13 +2,22 @@
 
 SDL_GLContext glContext;
 GLuint shaderProgram;
+GLuint VAO;
+GLuint VBO;
 
 void initOpenGL(SDL_Window* window) 
 {
+    glewInit();  // Initialize GLEW
+
+    glMatrixMode(GL_PROJECTION);  // Switch to projection matrix
+    glLoadIdentity();             // Reset the projection matrix
+    glOrtho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f);  // Create an orthographic projection
+    glMatrixMode(GL_MODELVIEW);   // Switch back to modelview matrix
+
+    //TODO: UPDATE RENDERING FOR 3.3
     // Set SDL attributes for OpenGL version and profile
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    // SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 
     // Create OpenGL context
     glContext = SDL_GL_CreateContext(window);
@@ -40,12 +49,45 @@ void initOpenGL(SDL_Window* window)
         exit(1);
     }
 
-    // Use the shader program
-    //glUseProgram(shaderProgram);
+    // Set uniform values
+    glUseProgram(shaderProgram);
+
+    // Set the distortion amount (adjust this value as necessary)
+    glUniform1f(glGetUniformLocation(shaderProgram, "distortionAmount"), 0.1f);
+
+    // Set the screen size (replace with your actual screen size)
+    glUniform2f(glGetUniformLocation(shaderProgram, "screenSize"), 800.0f, 600.0f);
+
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // Set the texture wrapping and filtering options
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Load and generate the texture
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(MENU_LOGO_IMAGE_PATH, &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+
+    // Bind the texture
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
 
     // Set the OpenGL viewport (typically after the context and shaders are set up)
-    int width, height;
-    SDL_GetWindowSize(window, &width, &height);  // Get window size
+    int windowWidth, windowHeight;
+    SDL_GetWindowSize(window, &windowWidth, &windowHeight);  // Get window size
     glViewport(0, 0, width, height);  // Set the OpenGL viewport to match the window size
 }
 
@@ -69,32 +111,27 @@ void renderQuadWithTexture(GLuint textureID, const SDL_Rect& rect)
         (GLfloat)(rect.x + rect.w), (GLfloat)(rect.y + rect.h), 1.0f, 1.0f // Top-right corner
     };
 
-    // Set up the indices (two triangles for the quad)
-    GLuint indices[] = { 0, 1, 2, 1, 2, 3 };
-
-    // Create VBOs (Vertex Buffer Objects) and VAOs (Vertex Array Objects)
-    GLuint VAO, VBO, EBO;
+    // Create Vertex Array Object (VAO) and Vertex Buffer Object (VBO)
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
+    // Bind the Vertex Array Object
     glBindVertexArray(VAO);
+
+    // Bind the VBO and upload the vertex data
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Position attribute
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)0);
+    // Specify the layout of the vertex data
+    // Position attribute (layout location = 0)
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // Texture coordinate attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)(2 * sizeof(GLfloat)));
+    // Texture coordinate attribute (layout location = 1)
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
 
-    // Draw the quad
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    // Clean up
+    // Unbind the VBO and VAO
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
     glBindVertexArray(0);
 }
